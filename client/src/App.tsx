2b4551890a1631ee00cbb49d5b2d83e4059875a9
johnnysee <js.johnny.see@gmail.com>
2022-11-18 23:43:06 +0100

    json to ts interface

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import { Product } from "./product";
 
 const URL = "https://localhost:5001/api/Products";
 
 export const App = () => {
+  const [products, setProducts] = useState<Product[]>([]);
   useEffect(() => {
     axios
       .get(URL)
-      .then((res) => console.log(res))
+      .then((res) => setProducts(res.data))
       .catch((e) => console.log(e));
   }, []);
-  return <></>;
+  return (
+    <>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
