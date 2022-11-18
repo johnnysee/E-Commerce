a4aa95e4ccdb9c3443eb0a02f586d97e675a2566
johnnysee <js.johnny.see@gmail.com>
2022-11-18 23:37:37 +0100

    fix cors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,14 @@
-import {
-  Card,
-  CardMedia,
-  CardContent,
-  Typography,
-  CardActions,
-  Button,
-} from "@mui/material";
-import data from "./data/products.json";
+import { useEffect } from "react";
+import axios from "axios";
+
+const URL = "https://localhost:5001/api/Products";
 
 export const App = () => {
-  return (
-    <>
-      {data &&
-        data.map((d) => (
-          <Card
-            key={d.id}
-            sx={{ maxWidth: 345, display: "flex", flexDirection: "row" }}
-          >
-            <CardMedia component="img" height="140" alt="green iguana" />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {d.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        ))}
-    </>
-  );
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((res) => console.log(res))
+      .catch((e) => console.log(e));
+  }, []);
+  return <></>;
 };
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://localhost:5001/",
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [react()]
+
 })
