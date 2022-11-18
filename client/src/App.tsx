6f44365bdee0bf60127f84143497c49142bdd887
johnnysee <js.johnny.see@gmail.com>
2022-11-18 22:44:35 +0100

    api with sdk and scaffolded client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,38 @@
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
+import data from "./data/products.json";
+
 export const App = () => {
-  return <h1>App</h1>;
+  return (
+    <>
+      {data &&
+        data.map((d) => (
+          <Card
+            key={d.id}
+            sx={{ maxWidth: 345, display: "flex", flexDirection: "row" }}
+          >
+            <CardMedia component="img" height="140" alt="green iguana" />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {d.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        ))}
+    </>
+  );
 };
---
+++ server/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using server.Entities;
+
+namespace server.Data
+{
+  public class StoreContext : DbContext
+  {
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+  }
+}
