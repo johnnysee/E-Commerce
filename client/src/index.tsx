fa570e42ecba94468eff3be6c117697cfa1207ea
johnnysee <js.johnny.see@gmail.com>
2022-11-21 20:37:18 +0100

    add react context to centralize basket states

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { App } from "./app/layout/App";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import { StoreProvider } from "./app/context/StoreContext";
 
 export const history = createBrowserHistory();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Router history={history}>
-      <App />
+      <StoreProvider>
+        <App />
+      </StoreProvider>
     </Router>
   </React.StrictMode>
 );
