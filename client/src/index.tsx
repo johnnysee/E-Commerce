d267ad677bc28b8005f0ee0f94ce2972fc45b89f
johnnysee <js.johnny.see@gmail.com>
2022-11-23 01:58:29 +0100

    using redux with redux toolkit instead of react context

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,16 +3,17 @@ import ReactDOM from "react-dom/client";
 import { App } from "./app/layout/App";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import { StoreProvider } from "./app/context/StoreContext";
+import { Provider } from "react-redux";
+import { store } from "./app/store/configureStore";
 
 export const history = createBrowserHistory();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Router history={history}>
-      <StoreProvider>
+      <Provider store={store}>
         <App />
-      </StoreProvider>
+      </Provider>
     </Router>
   </React.StrictMode>
 );
