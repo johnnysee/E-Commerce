8cd8aff5d7d236067e9a7a7a8eb59e3b73b5d20e
johnnysee <js.johnny.see@gmail.com>
2022-11-26 19:52:26 +0100

    adding filtering functionality

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -20,6 +20,7 @@ import {
 } from "@mui/material";
 import { ProductSearch } from "./ProductSearch";
 import { RadioButtonGroup } from "../../app/components/RadioButtonGroup";
+import { CheckboxButtons } from "../../app/components/CheckBoxButtons";
 
 const sortOptions = [
   { value: "name", label: "Alphabetical" },
@@ -68,27 +69,23 @@ export const Catalog = () => {
         </Paper>
 
         <Paper sx={{ mb: 2, p: 2 }}>
-          <FormGroup>
-            {brands.map((brand) => (
-              <FormControlLabel
-                control={<Checkbox />}
-                label={brand}
-                key={brand}
-              />
-            ))}
-          </FormGroup>
+          <CheckboxButtons
+            items={brands}
+            checked={productParams.brands}
+            onChange={(items: string[]) =>
+              dispatch(setProductParams({ brands: items }))
+            }
+          />
         </Paper>
 
         <Paper sx={{ mb: 2, p: 2 }}>
-          <FormGroup>
-            {types.map((type) => (
-              <FormControlLabel
-                control={<Checkbox />}
-                label={type}
-                key={type}
-              />
-            ))}
-          </FormGroup>
+          <CheckboxButtons
+            items={types}
+            checked={productParams.types}
+            onChange={(items: string[]) =>
+              dispatch(setProductParams({ types: items }))
+            }
+          />
         </Paper>
       </Grid>
       <Grid item xs={9}>
